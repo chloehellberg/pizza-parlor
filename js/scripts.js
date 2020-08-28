c9decd4b94e967beb86a8235d47ef22007d5c159
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-28 10:21:41 -0700

    Start on UI logic - attempt at gathering input values

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,51 @@
-// Create a website for a pizza company where a user can choose one or more individual toppings 
-//(cheese, pepperoni, artichoke, anchovy, etc) and a size to order a pizza and see the final cost.
+// //Business Logic
 
-// * Spec 2: Create a prototype method for the cost of the pizza based on selections. Use your own formula for this. 
+// function PizzaOrder () {
+//   this.toppingsChoice = [],
+//   this.sizeChoice = []
+// }
 
+// let pizzaOptions = {
+//   toppings: ["Potato", "Carmelized Onions", "Fresh Chives", "Sausage", "Sauteed Mushrooms", "Black Olives", "Basil", "Pepperoni"],
+//   size = ["Mini", "Medium", "Large"],
+// }
 
-//Pizza Object Constructor 
+// PizzaOrder.prototype.calculateCost = function (toppings, size) {
+//   let orderArray = [];
 
-function PizzaOrder () {
-  this.toppingsChoice = [],
-  this.sizeChoice = []
-}
-
-let pizzaOptions = {
-  toppings: ["Potato", "Carmelized Onions", "Fresh Chives", "Sausage", "Sauteed Mushrooms", "Black Olives", "Basil", "Pepperoni"],
-  size = ["Mini", "Medium", "Large"],
-}
-
-PizzaOrder.prototype.calculateCost = function (toppings, size) {
-  let orderArray = [];
-
-  for (let i = 0; i <= toppings.length; i++ )
+//   for (let i = 0; i <= toppings.length; i++ )
   
-}
+// };
 
 
-let order = new PizzaOrder()
-console.log(order.calculateCost());
-
-
-
+// let order = new PizzaOrder()
+// console.log(order.calculateCost());
 
 
 
 
+// User Interface Logic
+
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+
+      $("#toppings-choice").show();
+      $("input:checkbox[name=topping-option]:checked").each(function(){
+        const toppingsChoice = $(this).val();
+      $("#toppings-choice").append(toppingsChoice + "<br>");
+      });
+
+      
+
+  })
+})
+
+$("#work-responses").show();
+$("input:checkbox[name=work-transportation]:checked").each(function(){
+  const workTransportationMode = $(this).val();
+  $('#work-responses').append(workTransportationMode + "<br>");
+});
 
 
 
@@ -53,25 +66,31 @@ console.log(order.calculateCost());
 
 
 
-let sizeOptions = {
-  size1: "Small",
-  size2: "Medium",
-  size3: "Large"
-}
 
-let sizeOptions = {
-  size = ["Small", "Medium", "Large"],
-}
 
-let toppingOptions = {
-  topping1: "Potato",
-  topping2: "Carmelized Onions",
-  topping3: "Fresh Chives",
-  topping4: "Sausage",
-  topping5: "Sauteed Mushrooms",
-}
 
-let toppingOptions = {
-  toppings: ["Potato", "Carmelized Onions", "Fresh Chives", "Sausage", "Sauteed Mushrooms"]
-}
+
+
+
+// let sizeOptions = {
+//   size1: "Small",
+//   size2: "Medium",
+//   size3: "Large"
+// }
+
+// let sizeOptions = {
+//   size = ["Small", "Medium", "Large"],
+// }
+
+// let toppingOptions = {
+//   topping1: "Potato",
+//   topping2: "Carmelized Onions",
+//   topping3: "Fresh Chives",
+//   topping4: "Sausage",
+//   topping5: "Sauteed Mushrooms",
+// }
+
+// let toppingOptions = {
+//   toppings: ["Potato", "Carmelized Onions", "Fresh Chives", "Sausage", "Sauteed Mushrooms"]
+// }
 
