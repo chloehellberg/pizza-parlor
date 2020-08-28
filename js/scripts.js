9e81648b22554e938a8fe4fc56e40c2e2be32113
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-28 13:47:53 -0700

    Successful add of calculating total of toppingsChoice & sizeChoice arrays

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,25 @@ PizzaOrder.prototype.calculateCost = function (toppingsChoice, sizeChoice) {
   let sizeChoiceTotal = this.sizeChoice;
   let totalCost = 0;
 
-  for (let i = 0; i < toppingsChoiceTotal.)
+  let toppingsChoiceTotal = [4];
+  let sizeChoiceTotal = [2];
+  let totalCost = 0;
+  
+  for(let i=0; i< toppingsChoiceTotal.length; i++) {
+  totalCost += toppingsChoiceTotal[i]+sizeChoiceTotal[i];
+  }
 
 
 
-  var arr1 = [2,3,4,5];
-  var arr2 = [4,3,3,1];
-  var sum = 0;
-  for(var i = 0; i < arr1.length; i++){
-    sum.push(array1[i] + array2[i]);
- }
-s
+  let arr1 = [2];
+  let arr2 = [4];
+  let sum = 0;
+  for(var i=0; i< arr1.length; i++) {
+    sum += arr1[i]+arr2[i];
+}
+
+
+
 
 
 
