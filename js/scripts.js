911c4f8e67467b070259aa6ef77a3b07d72e10d8
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-28 08:59:48 -0700

    Initial build of pizza object constructor and pizzaOptions. Previous pizza option objects have been left in the code in case I would like to go a different route and will be erased later.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,51 @@
-// Create a website for a pizza company where a user can choose one or more individual toppings (cheese, pepperoni, artichoke, anchovy, etc) and a size to order a pizza and see the final cost.
+// Create a website for a pizza company where a user can choose one or more individual toppings 
+//(cheese, pepperoni, artichoke, anchovy, etc) and a size to order a pizza and see the final cost.
 
 // * Spec 1: Create a pizza object constructor with properties for toppings and size.
 //   * Input: 
 //   * Output: 
+
+let pizza = {
+
+}
+
+//Pizza Object Constructor - This will be how 
+
+function PizzaOrder () {
+  this.toppingsChoice = [],
+  this.sizeChoice = []
+}
+
+let pizzaOptions = {
+  toppings: ["Potato", "Carmelized Onions", "Fresh Chives", "Sausage", "Sauteed Mushrooms"],
+  size = ["Small", "Medium", "Large"],
+}
+
+
+
+
+
+
+
+let sizeOptions = {
+  size1: "Small",
+  size2: "Medium",
+  size3: "Large"
+}
+
+let sizeOptions = {
+  size = ["Small", "Medium", "Large"],
+}
+
+let toppingOptions = {
+  topping1: "Potato",
+  topping2: "Carmelized Onions",
+  topping3: "Fresh Chives",
+  topping4: "Sausage",
+  topping5: "Sauteed Mushrooms",
+}
+
+let toppingOptions = {
+  toppings: ["Potato", "Carmelized Onions", "Fresh Chives", "Sausage", "Sauteed Mushrooms"]
+}
+
