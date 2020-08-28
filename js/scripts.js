7ef17b5794e664f83b0912de09b78b31a0c9c545
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-28 14:39:28 -0700

    First attempt at using the calculateCost method on newOrder unsuccessful.

--- js/scripts.js
+++ js/scripts.js
@@ -12,40 +12,17 @@ PizzaOrder.prototype.calculateCost = function (toppingsChoice, sizeChoice) {
   
   for(let i=0; i< toppingsChoiceTotal.length; i++) {
     totalCost += toppingsChoiceTotal[i]+sizeChoiceTotal[i];
-    if (totalCost <= 18 && totalCost >= 15) {
+  }
+    if (totalCost === 13) {
       return ("This costs $30");
     } 
-    if (totalCost <= 14 && totalCost >= 10) {
+    if (totalCost >= 10) {
       return ("This costs $20");
     } 
     else {
       return ("This costs $10");
     }  
   }
-}
-
-
-
-PizzaOrder.prototype.calculateCost = function (toppingsChoice, sizeChoice) {
-  let toppingsChoiceTotal = [this.toppingsChoice.reduce((a,b) => a + b, 0)];
-  let sizeChoiceTotal = this.sizeChoice;
-  let totalCost = 0;
-  
-  for(let i=0; i< toppingsChoiceTotal.length; i++) {
-    if (totalCost += toppingsChoiceTotal[i]+sizeChoiceTotal[i]);
-      if (totalCost <= 18 && totalCost >= 15) {
-        return ("This costs $30");
-      } 
-      if (totalCost <= 14 && totalCost >= 10) {
-        return ("This costs $20");
-      } 
-      else {
-        return ("This costs $10");
-      }    
-  }
-}
-
-
 
 
 
@@ -61,6 +38,9 @@ $(document).ready(function() {
       newOrder.toppingsChoice.push(parseInt($(this).val()));
     });
       newOrder.sizeChoice.push(parseInt($("#size-option").val()));
+      
+      newOrder.calculateCost();
+      console.log(newOrder.calcuateCost());
   });   
 });
 
@@ -69,7 +49,25 @@ $(document).ready(function() {
 
 
 
-// let pizzaOptions = {
-//   toppings: ["Potato", "Carmelized Onions", "Fresh Chives", "Sausage", "Sauteed Mushrooms", "Black Olives", "Basil", "Pepperoni"],
-//   size = ["Mini", "Medium", "Large"],
-// }
+// PizzaOrder.prototype.calculateCost = function (toppingsChoice, sizeChoice) {
+//   // let toppingsChoiceTotal = [this.toppingsChoice.reduce((a,b) => a + b, 0)];
+//   // let sizeChoiceTotal = this.sizeChoice;
+//   let toppingsChoiceTotal = [10];
+//   let sizeChoiceTotal = [3]
+//   let totalCost = 0;
+  
+//   for(let i=0; i< toppingsChoiceTotal.length; i++) {
+//     totalCost += toppingsChoiceTotal[i]+sizeChoiceTotal[i];
+//   }
+//     if (totalCost === 13) {
+//       return ("This costs $30");
+//     } 
+//     if (totalCost >= 10) {
+//       return ("This costs $20");
+//     } 
+//     else {
+//       return ("This costs $10");
+//     }  
+//   }
+// let newOrder = new PizzaOrder();
+// newOrder.calculateCost();
